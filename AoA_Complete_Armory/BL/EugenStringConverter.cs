e7aae337c889d84885c952bd86c88e2b6cbc4369
mathieujaumain <ma.jaumain@gmail.com>
2015-06-11 18:25:41 +0200

    Description string converter

--- AoA_Complete_Armory/BL/EugenStringConverter.cs
+++ AoA_Complete_Armory/BL/EugenStringConverter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace DM.Armory.BL
 {
@@ -11,13 +14,39 @@ namespace DM.Armory.BL
     /// </summary>
     public class EugenStringConverter
     {
-        #region ColorTags
-        
+        #region Tags
+        private static string[] GREEN_TAG = new string[]{ "#styleGreen{", "}" } ;
+        private static string START_TAG = "#";
         #endregion
 
-        public static void Convert(string eugenStr)
+        public static void ConvertGreenText(string eugenStr, ref RichTextBox textBox)
         {
+            int i = 0;
+            string[] parts =  eugenStr.Split(GREEN_TAG, StringSplitOptions.None);
+            if (parts.Length > 1) 
+            {
+                if (i % 2 == 0 )
+                {
+                    textBox.AppendText(parts[i].Replace("#styleGreen", string.Empty));
+                }
+                else
+                {
+                    AppendTextWithColor(ref textBox, parts[i], Color.Teal);
+                }
+            }
 
+            textBox.Find(GREEN_TAG[0]);
+        }
+
+        public static void AppendTextWithColor(ref RichTextBox box, string text, Color color)
+        {
+            
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
         }
     }
 }
