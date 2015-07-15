ae92aad9a3723339b77e811ba8d351ea90ea4c15
mathieujaumain <ma.jaumain@gmail.com>
2015-07-15 18:19:24 +0200

    Add a model view to mainWindow

--- AoA_Complete_Armory/ViewModel/MainViewModel.cs
+++ AoA_Complete_Armory/ViewModel/MainViewModel.cs
@@ -3,26 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DM.Armory.Model;
 using DM.Armory.BL;
 
 namespace DM.Armory.ViewModel
 {
     public class MainViewModel
     {
-        private ObjectLibrary _Library;
-        public ObjectLibrary Library { get { return _Library; } }
-        
+        private InitializationController Init;
+        private List<BuildingViewModel> _cartelBuildings = new List<BuildingViewModel>();
+        private List<BuildingViewModel> _usBuildings = new List<BuildingViewModel>();
+        private List<BuildingViewModel> _chimBuildings = new List<BuildingViewModel>();
 
         public MainViewModel()
         {
-            InitializationController init = new InitializationController();
-            _Library = new ObjectLibrary(init.LoadData()); //...
+            Init = new InitializationController();
+            if (Init.LoadData())
+            {
+                foreach (AoABuilding b in Init.UsBuildings)
+                {
+                    BuildingViewModel buioding = new BuildingViewModel(b);
+                    _usBuildings.Add(buioding);
+                }
+                Init.UsBuildings.Clear();
+
+
+
+                //Cartel
+                foreach (AoABuilding b in Init.CartelBuildings)
+                {
+                    BuildingViewModel buioding = new BuildingViewModel(b);
+                    _cartelBuildings.Add(buioding);
+                }
+                Init.CartelBuildings.Clear();
+
+
+
+                //Chimera
+                foreach (AoABuilding b in Init.ChimeraBuildings)
+                {
+                    BuildingViewModel buioding = new BuildingViewModel(b);
+                    _chimBuildings.Add(buioding);
+                }
+                Init.ChimeraBuildings.Clear();
+
+
+            }
         }
 
-        public MainViewModel(long version)
-        {
-            InitializationController init = new InitializationController();
-            _Library = new ObjectLibrary(init.LoadData(version)); //...
-        }
     }
 }
